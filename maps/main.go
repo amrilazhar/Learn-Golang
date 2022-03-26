51ef61df3e66d4a9170d67ca68821427ee69f738
amrilazhar <amrilazhar@gmail.com>
2022-03-26 10:19:10 +0700

    learn map part2

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string]string
+
+	colors := make(map[int]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors[9] = "whitecolor"
+	colors[2] = "whitecolor2"
+
+	delete(colors, 2)
 
 	fmt.Println(colors)
 }
