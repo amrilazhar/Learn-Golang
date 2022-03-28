7ca947fa040c0334fcf1f9d431285155fec0025c
amrilazhar <amrilazhar@gmail.com>
2022-03-28 12:40:06 +0700

    update

--- interfaces/main.go
+++ interfaces/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type bot interface {
-	getGreeting() string
+	getGreeting(string) string
 	// getFarewell() string
 }
 
@@ -14,17 +14,17 @@ func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
 
-	printGreeting(eb)
-	printGreeting(sb)
+	printGreeting(eb, "hi")
+	printGreeting(sb, "hola")
 
 }
 
-func printGreeting(b bot) {
+func printGreeting(b bot, msg string) {
 	// call getgreeting
-	fmt.Println(b.getGreeting())
+	fmt.Println(b.getGreeting("start"))
 }
 
-func (eb englishBot) getGreeting() string {
+func (eb englishBot) getGreeting(s string) string {
 	// imagine there is complex code here
 	return "Hello there!"
 }
@@ -34,6 +34,6 @@ func (eb englishBot) getFarewell() string {
 	return "Goodbye"
 }
 
-func (sb spanishBot) getGreeting() string {
+func (sb spanishBot) getGreeting(s string) string {
 	return "Hola!"
 }
